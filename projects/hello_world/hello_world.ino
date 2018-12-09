e51287fd8194af6a4e253371818fae45c072bf28
Gergely Bekesi <bekegerg@gmail.com>
2018-12-09 15:19:20 +0100

    Restructure code

--- projects/hello_world/hello_world.ino
+++ projects/hello_world/hello_world.ino
@@ -13,7 +13,7 @@ int mode = 0;
 
 // time
 int milliseconds = 0;
-int seconds = 50;
+int seconds = 00;
 int minutes = 0;
 int hours = 11;
 auto timer = timer_create_default();
@@ -115,12 +115,16 @@ void blink_led(uint8_t pin){
   }
 }
 
+void turn_off_led(uint8_t pin) {
+  led_status[pin - 2] = false;
+  digitalWrite(pin, LOW);
+}
+
 void set_mode_0() {
   mode = 0;
   blink_counter = 0;
-  led_status[0] = false;
-  led_status[1] = false;
-  
+  turn_off_led(2);
+  turn_off_led(3);
 }
 
 void setup() {
@@ -141,6 +145,7 @@ void setup() {
   }
   pinMode(2, OUTPUT);
   pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
 
   FastLED.setBrightness(brightness);
   Serial.begin(9600);
@@ -152,15 +157,42 @@ void loop() {
   // LEDs
   FastLED.clear();
   
-  if(mode == 0) {
-    int color_change = check_button_click(COLOR);
-    if(color_change == 1) {
-      switch_color();
-    }
-    int brightness_change = check_button_click(BRIGHT);
-    if(brightness_change == 1) {
-      change_brightness();
-    }
+//  if(mode == 0) {
+//    int color_change = check_button_click(COLOR);
+//    if(color_change == 1) {
+//      switch_color();
+//    }
+//    int brightness_change = check_button_click(BRIGHT);
+//    if(brightness_change == 1) {
+//      change_brightness();
+//    }
+//  }
+  int color_change = 0;
+  int brightness_change = 0;
+  switch(mode) {
+    
+    case 0:
+      color_change = check_button_click(COLOR);
+      if(color_change == 1) {
+        switch_color();
+      }
+      brightness_change = check_button_click(BRIGHT);
+      if(brightness_change == 1) {
+        change_brightness();
+      }
+      break;
+    case 1:
+      color_change = check_button_click(COLOR);
+      if(color_change == 1) {
+        switch_color();
+      }
+      brightness_change = check_button_click(BRIGHT);
+      if(brightness_change == 1) {
+        change_brightness();
+      }
+      break;
+    default:
+      break;
   }
   for (int i = 0; i <= hours; i++) {
     leds[i] = available_colors[selected_color];
@@ -176,6 +208,7 @@ void loop() {
     blink_led(3);
   }
   blink_counter++;
+  digitalWrite(4, HIGH);
 
   int time_set = check_button_click(TIME);
   if(time_set == 2) {
